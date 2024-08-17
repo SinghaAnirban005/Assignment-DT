0841e952d8c4cdcd817758f8214b875d9a03e534
SinghaAnirban005 <anirbansingha20@gmail.com>
2024-08-18 00:09:30 +0530

    Adjustments in design

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const subheader_head = document.querySelector('.subheader-head');
 const subheader_subhead = document.querySelector(".subheader-subhead")
 const card1_header = document.querySelector(".card1-header")
 const card1_span = document.querySelector(".card1-span")
-const video = document.getElementById("video")
+// const video = document.getElementById("video")
 const video_dash = document.querySelector('.card1-video');
 const card2_img = document.querySelector('.card2-img')
 const card2_header = document.querySelector(".card2-header")
@@ -27,7 +27,7 @@ card1_span.textContent = data.tasks[0].assets[0].asset_description
 card2_header.textContent = data.tasks[0].assets[1].asset_title
 card2_span.textContent = data.tasks[0].assets[1].asset_description
 card3_header.textContent = data.tasks[0].assets[2].asset_title
-// video.src = data.tasks[0].assets[0].asset_content
+video_dash.src = data.tasks[0].assets[0].asset_content
 // video_dash.load();tex
 card3_span.textContent = data.tasks[0].assets[2].asset_description
 card4_header.textContent = data.tasks[0].assets[3].asset_title
